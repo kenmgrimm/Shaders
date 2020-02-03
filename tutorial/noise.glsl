d9f1d5ba89535d10e54a50c7e860cdefe4fd6a43
Ken Grimm <kenmgrimm@gmail.com>
2020-02-02 22:50:55 -0700

    Initial

---
+++ tutorial/noise.glsl
@@ -0,0 +1,23 @@
+/*
+* Perlin noise
+*/
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+float randomNoise(vec2 p){
+  return fract(6791.*sin(47.*p.x+p.y*9973.));
+}
+
+void main(){
+  vec2 position=gl_FragCoord.xy/u_resolution.xx;
+  if((position.x>1.)||(position.y>1.)){
+    discard;
+  }
+
+  float n=randomNoise(position);
+  gl_FragColor=vec4(vec3(n),1.);
+}
