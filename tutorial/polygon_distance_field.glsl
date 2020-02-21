d1cbba7f48990135451a11fa1c30befa01d08f9e
Ken Grimm <kenmgrimm@gmail.com>
2020-02-20 22:25:34 -0700

    Changed to centered and normalized to -.5...5 on axis with min res

--- tutorial/polygon_distance_field.glsl
+++ tutorial/polygon_distance_field.glsl
@@ -6,33 +6,34 @@ precision mediump float;
 #define TWO_PI 6.28318530718
 
 uniform vec2 u_resolution;
-uniform vec2 u_mouse;
 uniform float u_time;
 
 // Reference to
 // http://thndl.com/square-shaped-shaders.html
 
 void main(){
-  float minRes = min(u_resolution.x, u_resolution.y);
-  vec2 st=gl_FragCoord.xy/minRes;
+  // uv centered and resized to min resolution
+  // min res resizes to -.5...5, no chopping
+  // max res will be >.5
+  float minRes=min(u_resolution.x,u_resolution.y);
+  vec2 st=(gl_FragCoord.xy-.5*u_resolution.xy)/minRes;
+
   vec3 color=vec3(0.);
   float d=0.;
   
-  // Remap the space to -1. to 1.
-  st=st*2.-1.;
-  // st*=10.;
+  // st*=2.;
   
   // Number of sides of your shape
-  int N=3;
+  int N=5;
   
   // Angle and radius from the current pixel
   float a=atan(st.x,st.y)+PI;
   float r=TWO_PI/float(N);
   
-  // Shaping function that modulate the distance
+  // Shaping function that modulates the distance
   d=cos(floor(.5+a/r)*r-a)*length(st);
   
-  color=vec3(1.-smoothstep(.4,.41,d));
+  color=vec3(1.-smoothstep(.2,.21,d));
   // color = vec3(d);
   
   gl_FragColor=vec4(color,1.);
